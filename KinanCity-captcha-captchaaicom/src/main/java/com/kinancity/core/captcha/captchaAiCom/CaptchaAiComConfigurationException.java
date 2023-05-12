4dc5878cc5b45ff592bff1c2c701eb9499851011
Malte <42342921+ccev@users.noreply.github.com>
2023-05-12 23:05:14 +0200

    captcha ai

---
+++ KinanCity-captcha-captchaaicom/src/main/java/com/kinancity/core/captcha/captchaAiCom/CaptchaAiComConfigurationException.java
@@ -0,0 +1,25 @@
+package com.kinancity.core.captcha.captchaAiCom;
+
+import com.kinancity.core.captcha.CaptchaException;
+
+public class CaptchaAiComConfigurationException extends CaptchaException {
+
+	private static final long serialVersionUID = -1173353148853930289L;
+
+	public CaptchaAiComConfigurationException() {
+		super();
+	}
+
+	public CaptchaAiComConfigurationException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CaptchaAiComConfigurationException(String message) {
+		super(message);
+	}
+
+	public CaptchaAiComConfigurationException(Throwable cause) {
+		super(cause);
+	}
+
+}
